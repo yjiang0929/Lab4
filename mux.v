4650d761207488ec7481c1859b24338b497f7e4d
NickShermeister <ncsherman159@gmail.com>
2018-11-11 11:33:48 -0500

    Well it's in one piece...

--- mux.v
+++ mux.v
@@ -25,3 +25,18 @@ module mux3 // 3 inputs of adjustable width, 2 address pins
 	assign out = (select[1] ? in3 : (select[0] ? in2 : in1));
 
 endmodule
+
+module mux4 // 3 inputs of adjustable width, 2 address pins
+#(parameter width = 32)
+(
+	output [width-1:0] out, // Output
+	input [width-1:0] in1, // Inputs, with arbitrary depth
+	input [width-1:0] in2,
+	input [width-1:0] in3,
+	input [width-1:0] in4,
+	input [1:0] select // Address
+);
+
+	assign out = (select[1] ? (select[0] ? in4 : in3) : (select[0] ? in2 : in1))
+
+endmodule
